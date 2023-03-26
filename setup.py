b8d0f72f32f4228e2a2bbe176526cb235f2503c5
Ryo Yonetani <yonetani.vision@gmail.com>
2023-03-26 16:39:32 +0900

    Add macos support (#5)
    * install tf on mac
    
    * fix setup.py
    
    * update readme

--- setup.py
+++ setup.py
@@ -1,3 +1,24 @@
 from setuptools import setup
 
-setup()
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="numpy2tfrecord",
+    version="0.0.3",
+    description="Convert a collection of numpy data to tfrecord",
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    url="https://github.com/yonetaniryo/numpy2tfrecord",
+    keywords="numpy, tfrecord, tensorflow",
+    author="Ryo Yonetani",
+    author_email="yonetani.vision@gmail.com",
+    license="MIT License",
+    packages=["numpy2tfrecord"],
+    install_requires=[
+        "numpy>=1.22.0",
+        "tensorflow;platform_system=='Windows'",
+        "tensorflow;platform_system=='linux'",
+        "tensorflow-macos;platform_system=='darwin'",
+    ],
+)
